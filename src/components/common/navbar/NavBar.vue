c437d91834804c640819dd63ba9ae12f0c73686e
cw <1814048443@qq.com>
2022-02-16 10:17:14 +0800

    组件构造

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style>
+  
+</style>
